6af7721e6c7baa74cb18a0261f999215d82abb30
Khusharth <khusharth19@gmail.com>
2019-09-06 21:27:54 +0530

    Connected CardList App SearchBox using State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 //Using { robots } as we are passing more than 1 value i.e (10 robots)
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        {/* Card List is accepting a robots props */}
-        <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filterRobots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return (
         <div className='pa2'>
             <input 
-            className='pa3 ba b--green bg-lightest-blue' 
-            type='Search' 
-            placeholder='Search robots' 
+                className='pa3 ba b--green bg-lightest-blue' 
+                type='Search' 
+                placeholder='Search robots' 
+                onChange={searchChange}
+                // OnChange call the searchchange function
             />
         </div>
     );
