3f9004e50fc0a2956037261eba23402bbf619087
Khusharth <khusharth19@gmail.com>
2019-09-06 11:45:36 +0530

    Added Card Component

---
+++ src/robots.js
@@ -0,0 +1,27 @@
+// This file can have multiple exports (Here we only have 1)
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@nelissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clenentine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patrick Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    }
+]
